c924b020de6abd44414d6e58e49b7f132bfab965
lordorito <maikolpedroza0@gmail.com>
2021-04-18 14:06:04 -0500

    js done

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const options = Array.from(d.getElementsByTagName('label'))
 
 function onClick() {
     this.classList.toggle("selected")
+
     for (const option of options) {
         if (option === this) continue
         else if (option.classList.contains("selected"))
